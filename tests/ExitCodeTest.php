67194f57b155a030e0f1d3e4e7a88bf810df840d
Hafiz Muhammad Moaz <hafizmoazkhalid@gmail.com>
2026-09-07 14:52:41 +0500

    style: fix ExitCodeTest header order

--- tests/ExitCodeTest.php
+++ tests/ExitCodeTest.php
@@ -9,6 +9,14 @@
  * file that was distributed with this source code.
  */
 declare(strict_types=1);
+/*
+ * This file is part of Webisters CLI Library.
+ *
+ * (c) Hafiz Muhammad Moaz <thewebisters@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 namespace Tests\CLI;
 
 use PHPUnit\Framework\TestCase;
